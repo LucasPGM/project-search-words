347030c2463109a273a20c67c9fae0a2cf651e7a
Lucas Soares <lucassoares@adxspace.com.br>
2022-07-07 18:07:51 -0300

    Add configs

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const router = require('./routes/index');
 
 const app = express();
 dotenv.config();
 
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', router);
 
 
 app.listen(process.env.PORT, ()=> {
