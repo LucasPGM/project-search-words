9146a702740739d67d9ce16a96587cc55c3cc4b4
Lucas Soares <lucassoares@adxspace.com.br>
2022-07-07 19:38:29 -0300

    Rendering the page ejs in the route

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,8 @@ const axios = require('axios').default;
 
 const router = express.Router();
 
-router.post('/word', (req, res) => {
-    try{
-        const word = req.body;
-        axios.get(`https://api.datamuse.com/words?sl=${word}`).then(response => {
-            res.send(response.data);
-        })
-
-    }catch (error) {
-        res.json(error);
-    }
+router.get('/word', (req, res) => {
+        res.render('sound');
 });
 
 
